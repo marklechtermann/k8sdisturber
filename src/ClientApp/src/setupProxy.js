2fb0db197ba1978e0a2dd1df9bc89259312dd8b8
Mark Lechtermann <mark.lechtermann@gmail.com>
2021-11-21 17:55:30 +0100

    Lockfile updated

--- src/ClientApp/src/setupProxy.js
+++ src/ClientApp/src/setupProxy.js
@@ -7,7 +7,7 @@ const target = env.ASPNETCORE_HTTPS_PORT
   ? env.ASPNETCORE_URLS.split(";")[0]
   : "http://localhost:56549";
 
-const context = ["/weatherforecast", "/api", "/swagger"];
+const context = ["/api", "/swagger"];
 
 module.exports = function (app) {
   const appProxy = createProxyMiddleware(context, {
