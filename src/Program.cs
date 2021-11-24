a270061e5dbbb3156a3cadb3cfd6d23710dc69d7
Mark Lechtermann <mlechtermann@dspace.de>
2021-11-24 14:30:52 +0100

    index.html fallback added

--- src/Program.cs
+++ src/Program.cs
@@ -39,6 +39,8 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
 
+app.MapFallbackToFile("index.html");
+
 var appService = app.Services.GetService<AppService>();
 if (appService == null) throw new InvalidOperationException();
 appService.Initialize();
