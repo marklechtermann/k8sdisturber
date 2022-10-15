9138b042e4b3b47e6dfc07247b0424f44a1bfdaa
Mark Lechtermann <mark.lechtermann@gmail.com>
2022-10-15 22:13:11 +0200

    feat: local storage support

---
+++ src/ClientApp/src/hooks/useThemeColor.ts
@@ -0,0 +1,30 @@
+import { PaletteMode } from "@mui/material";
+import { useState } from "react";
+
+export default function useTheme(): [string | PaletteMode, Function] {
+  const getValue = () => {
+    let themeColor = window.localStorage.getItem("themeColor");
+    if (themeColor != "dark" && themeColor != "light") {
+      themeColor = "light";
+    }
+
+    return themeColor as PaletteMode;
+  };
+
+  const setValue = (value: PaletteMode) => {
+    window.localStorage.setItem(
+      "themeColor",
+      getValue() == "light" ? "dark" : "light"
+    );
+  };
+
+  const [themeColor, setThemeColor] = useState<PaletteMode>(getValue());
+
+  return [
+    themeColor,
+    () => {
+      setValue(getValue() == "light" ? "dark" : "light");
+      setThemeColor(getValue());
+    },
+  ];
+}
